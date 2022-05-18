b9973e630a3e8d59cb018418756b89578141a90f
aopecho <aopecho@uchicago.edu>
2022-05-18 05:58:23 +0000

    adding actors table and fixing failed tests

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,11 +1,17 @@
 class ActorsController < ApplicationController
   def index
+    @list_of_actors = Actor.all
     render({ :template => "actor_templates/index.html.erb"})
   end
 
   def actor_details
     actor_id = params.fetch("actor_id")
-    @actor = Actor.where({ :id => actor_id })
+    #which actor?
+    @actor = Actor.where({ :id => actor_id }).at(0)
+    #roles by id
+    @roles = Character.where({ :actor_id => actor_id })
+    #movies gleaned from character roles
+    @list_of_movies = Movie.all
     render({ :template => "actor_templates/details.html.erb"})
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,7 @@ class MoviesController < ApplicationController
   def movie_details
     movie_id = params.fetch("movie_id")
     @movie = Movie.where({ :id => movie_id }).at(0)
-    @movie_director = Director.where({ :id => @movie.director_id }).at(0)
+    @movie_director = Director.where({ :id => @movie.director_id }).at(0).name
     render({ :template => "movie_templates/details.html.erb"})
   end
 end
